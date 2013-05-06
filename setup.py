3a67ea89a1513f1639b77becdbc32c124a13edc1
David Francos (XayOn) <xayon@xayon.net>
2013-05-06 20:22:57 +0200

    Moved to server.py the main server, so the setup works best, also, this enables debug when calling it directly

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# For some reason, building exes with py2exe doesnt work right now.
+from setuptools import setup
+from distutils.command.install import INSTALL_SCHEMES
+import os
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+data_files = []
+
+for dirpath, dirnames, filenames in os.walk('calentic'):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+setup(name='Calentic',
+      version='0.0.1',
+      description='Calendario TIC Aragón',
+      url='http://www.calenti.co',
+      download_url='http://github.com/Dlabs-co/calentic',
+      license='GPL2',
+      requires=['flask'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+      ],
+      long_description="Calendario TIC Aragón",
+      packages=['calentic'],
+      data_files=data_files,
+      package_data={
+        'calentic' : [
+            'static/'
+            'templates/'
+            ]
+      },
+      entry_points="""
+        [console_scripts]
+        rstextme = calentic.server:server
+      """
+     )
