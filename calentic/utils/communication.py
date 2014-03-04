631c31af527b464e839688ecd901c74ed49f10c7
David Francos Cuartero <work@davidfrancos.net>
2014-03-04 19:05:37 +0100

    Full refactor, same stuff, basically, but with nicer code

---
+++ calentic/utils/communication.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from mailer import Mailer, Message
+from flask import render_template
+from twitter import *
+
+class Sender(object):
+    """Communication engine"""
+
+    def __init__(self, config):
+        """
+        Loads configuration, and logs in with twitter and mailer
+
+        :config: configuration array
+
+        """
+        self._config = config
+
+        self.twitter = Twitter(auth=OAuth(
+            self._config['twitter']['oauth'][0]
+            self._config['twitter']['oauth'][1],
+            self._config['twitter']['oauth'][2],
+            self._config['twitter']['oauth'][3]
+        ))
+
+        self.sender = Mailer('smtp.gmail.com', use_tls=True, port=587)
+        self.sender.login(self._config['mail']['address'], self.config['mail']['pass'])
+
+    def send_mail(self, html):
+        """
+            Send a mail via smtps
+        """
+        message = Message(
+            From=self._config['mail']['address'], To=self._config['mail']['to'],
+            Subject=self._config['mail']['subject']
+        )
+        message.Html = html
+        return self.sender.send(message)
+
+    def publish_twitter(event, event_url):
+        """
+            Simple twitter status update with the event url and title
+        """
+        return self.twitter.statuses.update(status='%s: %s' %(event, event_url))
---
+++ calentic/utils/utils.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+    File: utils.py
+    Author: David Francos
+    Description: Stuff that I'm not sure where to put
+'''
+
+import json as _json
+import time
+import datetime
+from time import strptime, mktime
+from dateutil import parser
+from flask import render_template
+from bson.objectid import ObjectId
+
+class Event(object):
+    """Event"""
+
+    def __init__(self, raw_event):
+        """
+            :raw_event: raw event data, unprocessed
+        """
+        self._raw_event = raw_event
+
+    def get_formatted_html(self):
+        external_url = self._raw_event.get('externalurl', False)
+        event = self.format_data()
+        if external_url:
+            event["url"] = "<a href='" + externalurl + "'>" + externalurl + "</a>"
+        else:
+            event["url"] = "<a href='" + event['url'] + "'>Más información</a>"
+
+        return render_template("single_event.html", event=event)
+
+    def get(self, element, default="", append="", link=False):
+        """
+            Return default for object_[element] with the extra append stuff
+        """
+        result = self._raw_event.get(element, "")
+        if result != "":
+            return result + append
+        elif link:
+            return "<a href=" + link + ">" + default + "</a>"
+        return default
+
+    def format_data(self):
+        """
+            Return the event with default values.
+        """
+
+        return {
+            "title" : self.get('title', ""),
+            "origin_url": self.get('origin_url', ""),
+            "origin_name": self.get('origin', ""),
+            "url"   : '/event/' + str(self._raw_event['_id']),
+            "externalurl" : self.get("url", ""),
+            'location' : self.get("location", ""),
+            'description': self.get("description", "Leer más", "...", self.get("url", False)),
+            "start" : time.mktime(parser.parse(self.get('start_date')).timetuple()) * 1000,
+            "end"   : time.mktime(parser.parse(self.get('end_date')).timetuple()) * 1000,
+            "class" : 'event-warning'
+        }
+
+    def __repr__(self):
+        return _json.dumps(self.format_data())
+
+class JSONEncoder(_json.JSONEncoder):
+    """
+       Replacing jsonencoder so it can handle objectids
+    """
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return ""
+        return _json.JSONEncoder.default(self, o)
+
+
+def dateformat(date):
+    """
+        Format date in a more web appropiate way
+    """
+    return datetime.datetime.fromtimestamp(int(int(date) / 1000)).strftime('%Y-%m-%d %H:%M:%S')
+
+
